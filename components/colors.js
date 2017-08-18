a690ed756ded7066a7b20bb1cc7be22be16a6533
Mustafa Çelik <celikmus@gmail.com>
2017-08-18 11:16:28 +0100

    Upgraded to Expo SDK 19.0 and ran prettier on js modules

--- components/colors.js
+++ components/colors.js
@@ -2,12 +2,12 @@
  * Created by ggoma on 12/24/16.
  */
 export default {
-    brown: '#753D22',
-    light_brown: '#F3C074',
-    dark_brown: '#4E2816',
-    orange: '#F0693E',
-    red: '#D52026',
-    dark_red: '#991C1E',
-    yellow: '#FFD932',
-    bag: '#991C1E'
-}
+  brown: '#753D22',
+  light_brown: '#F3C074',
+  dark_brown: '#4E2816',
+  orange: '#F0693E',
+  red: '#D52026',
+  dark_red: '#991C1E',
+  yellow: '#FFD932',
+  bag: '#991C1E'
+};
--- components/scale.js
+++ components/scale.js
@@ -3,17 +3,17 @@
  */
 
 //change this to resize the chicken
-const SCALE_SIZE = .7;
+const SCALE_SIZE = 0.7;
 
-export const whole = [{scale: SCALE_SIZE}];
-export const eyes = [{scale: .5}];
-export const tail = [{scale: .8}];
-export const tail2 = [{rotate: '-50deg'},{scale: .8}];
-export const beak = [{scale: .6}];
-export const bag = [{scale: .6}];
-export const hair = [{rotate: '-80deg'},{scale: .5}];
-export const hair2 = [{rotate: '190deg'},{scale: .5}];
-export const hair3 = [{rotate: '0deg'},{scale: .5}];
-export const hair4 = [{rotate: '-110deg'},{scale: .5}];
-export const leg = [{rotate: '20deg'}];
-export const leg2 = [{rotate: '-20deg'}];
+export const whole = [{ scale: SCALE_SIZE }];
+export const eyes = [{ scale: 0.5 }];
+export const tail = [{ scale: 0.8 }];
+export const tail2 = [{ rotate: '-50deg' }, { scale: 0.8 }];
+export const beak = [{ scale: 0.6 }];
+export const bag = [{ scale: 0.6 }];
+export const hair = [{ rotate: '-80deg' }, { scale: 0.5 }];
+export const hair2 = [{ rotate: '190deg' }, { scale: 0.5 }];
+export const hair3 = [{ rotate: '0deg' }, { scale: 0.5 }];
+export const hair4 = [{ rotate: '-110deg' }, { scale: 0.5 }];
+export const leg = [{ rotate: '20deg' }];
+export const leg2 = [{ rotate: '-20deg' }];
